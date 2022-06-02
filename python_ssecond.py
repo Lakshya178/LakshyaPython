2b578c48e771e2776b4df89f54975859393fdc99
Lakshya178 <52889423+Lakshya178@users.noreply.github.com>
2022-06-02 16:11:06 +0530

    changed variable names

--- python_ssecond.py
+++ python_ssecond.py
@@ -1,11 +1,11 @@
-digits = 234
-dispatch = { "2":'abc',  "3":'def',\
+digits_given = 234
+numbers_vs_characters = { "2":'abc',  "3":'def',\
                      "4":'ghi',  "5":'jkl',\
                      "6":'mno',  "7":'pqrs',\
                      "8":'tuv',  "9":'wxyz'}
 output = [""]
-for d in str(digits):
-    output = [i+j for i in output for j in dispatch[d]]
+for d in str(digits_given):
+    output = [i+j for i in output for j in numbers_vs_characters[d]]
     print(output)
 if output == [""]:
     output = [] 
